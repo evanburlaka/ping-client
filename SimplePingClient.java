6886d45a9322befcd2bfe51f47bac247e87f3c6c
evanburlaka <evanb3002@gmail.com>
2025-04-27 01:34:09 -0700

    Add files via upload

---
+++ SimplePingClient.java
@@ -0,0 +1,27 @@
+//Evan Burlaka - Simple Ping Client
+import java.net.*;
+
+public class SimplePingClient {
+	public static void main(String[] args) {
+		if (args.length != 1) { //prevent crash check in case of empty argument
+			System.out.println("missing hostname");
+			return;
+		}
+
+		String host = args[0]; //retrieve first commandline arg
+
+		try {
+			InetAddress addr = InetAddress.getByName(host);
+			boolean reachable = addr.isReachable(3000); 
+			// check if reachable within 3 seconds
+
+			if (reachable) {
+				System.out.println(host + " is reachable.");
+			} else {
+				System.out.println(host + " is not reachable.");
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
---
+++ TimedPingClient.java
@@ -0,0 +1,38 @@
+//En Burlaka - Timed Ping Client
+import java.net.*;
+
+public class TimedPingClient {
+	public static void main(String[] args) {
+		if (args.length != 1) { //prevent crash check in case of empty argument
+			System.out.println("missing hostname");
+			return;
+		}
+		String host = args[0];
+		try {
+			InetAddress addr = InetAddress.getByName(host); //take host IP as object
+			DatagramSocket socket = new DatagramSocket(); //UDP socket
+			socket.setSoTimeout(3000);
+			System.out.println("Waiting...");
+			// wait 3 seconds for response
+			byte[] message = "hello1234".getBytes(); //send "hello1234" instead of empty packet
+			DatagramPacket request = new DatagramPacket(message, message.length, addr, 7);
+			//UDP packet created
+			long trackSendTime = System.currentTimeMillis(); //Start tracking time to send
+			socket.send(request); //UDP packet sent on port 7
+			byte[] dataForResp = new byte[512]; //new buffer for response
+			DatagramPacket response = new DatagramPacket(dataForResp, dataForResp.length);
+			try {
+				socket.receive(response); //waiting for response
+				long trackRecTime = System.currentTimeMillis(); // track time for recieve
+				long rtt = trackRecTime - trackSendTime; //total length of time (ms)
+				System.out.println(host + " is reachable. RTT: " + rtt + " ms");
+			} catch (SocketTimeoutException e) {
+				System.out.println(host + " is not reachable.");
+			}
+			socket.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
